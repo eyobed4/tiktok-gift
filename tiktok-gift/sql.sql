6fb787305249d52855b400374d38a1aadb3c421b
eyobed4 <litecoin4444@gmail.com>
2025-08-24 00:42:42 +0300

    Delete tiktok-gift directory

--- tiktok-gift/sql.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE tiktok_clone;
-CREATE USER 'tiktok_user'@'localhost' IDENTIFIED BY 'secure_password';
-GRANT ALL PRIVILEGES ON tiktok_clone.* TO 'tiktok_user'@'localhost';
-FLUSH PRIVILEGES;
